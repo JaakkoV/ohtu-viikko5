33458d04eb113024cd2c0470100b8fdf13ced74d
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-04-22 16:45:12 +0300

    create jacoco

--- VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -210,5 +210,32 @@ public class KauppaTest {
         verify(pankki).tilisiirto(eq("gagga"), eq(44), anyString(), anyString(), anyInt());
         verify(viite, times(3)).uusi();
     }
+    @Test
+    public void poistaKorista() {
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42).thenReturn(43).thenReturn(44);
+
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leipä", 7));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);    
+        k.poistaKorista(1);
+        k.tilimaksu("pekka", "12345");
+        
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(7));
+    }
 
 }
