4ab3f7cae0157b6ade4bbc561d954077d9996ad7
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-04-22 16:20:58 +0300

    exercise 3: test 2

--- VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -1,4 +1,5 @@
 package ohtu.verkkokauppa;
+
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
@@ -30,4 +31,38 @@ public class KauppaTest {
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(5));
         // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
+
+    @Test
+    public void aloitetaanAsiointiJaLisataanKaksiEriTuotetta() {
+        /* aloitetaan asiointi, koriin lisätään kaksi eri tuotetta, joita varastossa on ja 
+        suoritetaan ostos. varmistettava että kutsutaan pankin metodia tilisiirto oikealla 
+        asiakkaalla, tilinumerolla ja summalla */
+
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.saldo(2)).thenReturn(20);
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leipä", 7));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);     // ostetaan tuotetta numero 2 eli leipää
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(12));
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+
 }
