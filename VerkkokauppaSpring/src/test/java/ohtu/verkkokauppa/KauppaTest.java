003ad3158d8e103974422f60406647f33b9b2881
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-04-22 16:34:34 +0300

    exercise 3: test 5

--- VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -129,4 +129,43 @@ public class KauppaTest {
         // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
 
+    @Test
+    public void aloitaAsiointiNollaaOstoksenTiedot() {
+        /* varmistettava, että metodin aloitaAsiointi kutsuminen nollaa edellisen 
+        ostoksen tiedot (eli edellisen ostoksen hinta ei näy uuden ostoksen hinnassa), 
+        katso tarvittaessa apua projektin MockitoDemo testeistä! */
+
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42).thenReturn(43);
+
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leipä", 7));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+        
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(12));
+        
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("kekka", "123456");
+        
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("kekka"), eq(43), eq("123456"), anyString(), eq(7));
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+
 }
