68c4fca19cccfd40067e98771ff51762f9ce6e5d
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-04-22 16:41:54 +0300

    exercise 3: test 6

--- VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -168,4 +168,47 @@ public class KauppaTest {
         // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
     }
 
+    @Test
+    public void uusiViitenumeroJokaOstokselle() {
+        /* varmistettava, että kauppa pyytää uuden viitenumeron jokaiselle maksutapahtumalle
+        , katso tarvittaessa apua projektin MockitoDemo testeistä! */
+
+        // luodaan ensin mock-oliot
+        Pankki pankki = mock(Pankki.class);
+        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42).thenReturn(43).thenReturn(44);
+
+        Varasto varasto = mock(Varasto.class);
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.saldo(2)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "leipä", 7));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+        
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(), eq(12));
+        
+        k.aloitaAsiointi();
+        k.lisaaKoriin(2);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("kekka", "123456");
+        
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("kekka"), eq(43), eq("123456"), anyString(), eq(7));
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+        
+        k.aloitaAsiointi();
+        k.tilimaksu("gagga", "12421");
+        verify(pankki).tilisiirto(eq("gagga"), eq(44), anyString(), anyString(), anyInt());
+        verify(viite, times(3)).uusi();
+    }
+
 }
