f71dca7438f8dabcd1ffcd4472399a751fac8d55
JaakkoV <jaakvirt@cs.helsinki.fi>
2017-04-23 10:26:25 +0300

    few minor optimization

--- Tennis/src/main/java/ohtu/ScoreMonitor.java
+++ Tennis/src/main/java/ohtu/ScoreMonitor.java
@@ -4,28 +4,19 @@ import java.util.HashMap;
 
 public class ScoreMonitor {
 
-    private static final HashMap<Integer, String> pointsToText = createPointsToText();
-
-    private static HashMap<Integer, String> createPointsToText() {
-        HashMap<Integer, String> pointsText = new HashMap<>();
-        pointsText.put(0, "Love");
-        pointsText.put(1, "Fifteen");
-        pointsText.put(2, "Thirty");
-        pointsText.put(3, "Forty");
-        return pointsText;
-    }
+    private static final String[] pointsToText = new String[] {"Love","Fifteen", "Thirty", "Forty"};
 
     public static String printScore(Player first, Player second) {
         if (first.getPoints() == second.getPoints()) {
             return (first.getPoints() < 4)
-                    ? pointsToText.get(first.getPoints()) + "-All"
+                    ? pointsToText[first.getPoints()] + "-All"
                     : "Deuce";
         } else if (first.getPoints()
                 >= 4 || second.getPoints() >= 4) {
             String lead = first.getPoints() > second.getPoints() ? first.getName() : second.getName();
             return (Math.abs(first.getPoints() - second.getPoints()) > 1) ? "Win for " + lead : "Advantage " + lead;
         } else {
-            return pointsToText.get(first.getPoints()) + "-" + pointsToText.get(second.getPoints());
+            return pointsToText[first.getPoints()] + "-" + pointsToText[second.getPoints()];
         }
     }
 
